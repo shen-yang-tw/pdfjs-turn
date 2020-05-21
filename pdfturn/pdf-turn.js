a34aa9cdee76adf95c6d11c9355f766a97294d0b
Shen Yang <shen.yang.work.tw@gmail.com>
2020-05-21 17:05:41 +0800

    0521

--- pdfturn/pdf-turn.js
+++ pdfturn/pdf-turn.js
@@ -182,7 +182,7 @@
           viewer._getVisiblePages = this._visPages;
 
           PDFViewerApplication.eventBus._listeners.switchspreadmode = this._evSpread;
-          viewer.spreadMode = this._spreadBk;
+          // viewer.spreadMode = this._spreadBk;
           PDFViewerApplication.open();
 
           $('#viewer .page').removeAttr('style');
