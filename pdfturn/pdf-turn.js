4ccc1e072726dc746b43894a1b674d60bb4e22f4
Shen Yang <shen.yang.work.tw@gmail.com>
2020-05-22 13:30:15 +0800

    0522

--- pdfturn/pdf-turn.js
+++ pdfturn/pdf-turn.js
@@ -195,7 +195,8 @@
             $(this).css('width', parent._size(page, 'width')).css('height', parent._size(page, 'height'));
           });
 
-          PDFViewerApplication.pdfViewer.forceRendering();
+          PDFViewerApplication.pdfViewer.scrollPageIntoView();
+          PDFViewerApplication.pdfViewer._getVisiblePages();
           // var viewer = PDFViewerApplication.pdfViewer;
           // viewer.scrollPageIntoView = this._intoView;
           // viewer._getVisiblePages = this._visPages;
